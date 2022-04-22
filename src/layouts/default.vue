3a32770b04155cbac56a52370f36ac35c614c7cf
Imam Susanto <abc.imams@gmail.com>
2022-04-22 09:38:24 +0700

    Fix squeezed image

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -34,12 +34,13 @@ if ('quicklink' in window) {
     </Head>
     <header class="pb-30">
       <div class="flex items-start max-w-[38rem] mx-auto w-full gap-6">
-        <picture>
+        <picture class="flex-none">
           <source type="image/avif" :srcset="`/images/avatar.avif 1x, /images/avatar@2x.avif 2x`">
           <source type="image/webp" :srcset="`/images/avatar.webp 1x, /images/avatar@2x.webp 2x`">
-          <img class="rounded-full h-[4.5rem] w-[4.5rem] flex-none" :srcset="`/images/avatar.png 1x, /images/avatar@2x.png 2x`" alt="Imam Susanto's Portrait">
+          <img class="rounded-full h-[4.5rem] w-[4.5rem]" :srcset="`/images/avatar.png 1x, /images/avatar@2x.png 2x`"
+            alt="Imam Susanto's Portrait">
         </picture>
-        <div class="font-medium leading-normal">
+        <div class="font-medium leading-normal flex-auto">
           <router-link to="/">Imam Susanto</router-link>
           <div class="text-gray-500 dark:text-gray-400">Web Developer</div>
           <div class="text-gray-500 dark:text-gray-400">Tangerang Selatan, Banten</div>
