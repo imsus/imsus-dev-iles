cb9773ca412b00ec1a1331a6f02d695274c563b3
Imam Susanto <abc.imams@gmail.com>
2022-04-22 09:27:29 +0700

    Update image

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -27,14 +27,21 @@ if ('quicklink' in window) {
 
 <template>
   <div id="default-layout" class="px-6 py-[2rem] lg:py-[5rem]">
+
     <Head>
       <meta property="author" :content="$site.author">
       <html lang="id-ID" class="scroll-smooth" />
-      <body class="dark:bg-black dark:text-white antialiased font-sans"></body>
+
+    <body class="dark:bg-black dark:text-white antialiased font-sans"></body>
     </Head>
     <header class="pb-30">
       <div class="flex items-start max-w-[38rem] mx-auto w-full gap-6">
-        <img class="rounded-full h-[4.5rem] w-[4.5rem] flex-none" src="../assets/avatar.png" alt="">
+        <picture>
+          <source type="image/avif" srcset="/images/avatar.avif 1x, /images/avatar@2x.avif 2x">
+          <source type="image/webp" srcset="/images/avatar.webp 1x, /images/avatar@2x.webp 2x">
+          <img class="rounded-full h-[4.5rem] w-[4.5rem] flex-none"
+            srcset="/images/avatar.png 1x, /images/avatar@2x.png 2x" alt="">
+        </picture>
         <div class="font-medium leading-normal">
           <router-link to="/">Imam Susanto</router-link>
           <div class="text-gray-500 dark:text-gray-400">Web Developer</div>
